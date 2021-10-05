7fc41942798ed1950add328fefb78e37d3d0286f
agwara <nnaemekaagwara@gmail.com>
2021-10-05 01:33:09 +0100

    new update

--- src/components/StudentProfile/StudentProfile.jsx
+++ src/components/StudentProfile/StudentProfile.jsx
@@ -7,7 +7,14 @@ import styles from "./StudentProfile.module.css"
 const data = {
   "image": stutendImage,
   "department": "statistics",
-  "matricNum": "STA/19/7009"
+  "matricNum": "STA/19/7009",
+  "firstName": "Alonso",
+  "lastName": "Xabi",
+  "email": "xabialonso@gmail.com",
+  "phone": "+2347064830441",
+  "parentPhone": "+2348054672914",
+  "religion": "Atheist",
+  "address": "Block 23A, Ixtapa Avenue, Mexico"
 }
 
 const StudentProfile = () => {
@@ -41,7 +48,36 @@ const StudentProfile = () => {
         <div onClick={goToEdit} className={styles.editBtn}>Edit Profile</div>
       </div>
 
-      <div className={styles.mainDetails}></div>
+      <div className={styles.mainDetails}>
+        <div className={styles.detailText}>
+          <span className={styles.title}>First name</span>
+          <p className={styles.titleValue}>{data["firstName"]}</p>
+        </div>
+        <div className={styles.detailText}>
+          <span className={styles.title}>Last name</span>
+          <p className={styles.titleValue}>{data["lastName"]}</p>
+        </div>
+        <div className={styles.detailText}>
+          <span className={styles.title}>Email</span>
+          <p className={styles.titleValue}>{data["email"]}</p>
+        </div>
+        <div className={styles.detailText}>
+          <span className={styles.title}>Phone Number</span>
+          <p className={styles.titleValue}>{data["phone"]}</p>
+        </div>
+        <div className={styles.detailText}>
+          <span className={styles.title}>Parent Phone Number</span>
+          <p className={styles.titleValue}>{data["parentPhone"]}</p>
+        </div>
+        <div className={styles.detailText}>
+          <span className={styles.title}>Address</span>
+          <p className={styles.titleValue}>{data["address"]}</p>
+        </div>
+        <div className={styles.detailText}>
+          <span className={styles.title}>Religion</span>
+          <p className={styles.titleValue}>{data["religion"]}</p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,11 +1,55 @@
 import React from "react"
+import Chart from "react-apexcharts"
 
 import StudentInfo from "../../components/StudentInfo/StudentInfo"
 import StudentProfile from "../../components/StudentProfile/StudentProfile"
 
 import profileStyles from "./Profile.module.css"
 
+
 const Profile = () => {
+  let data = {
+    options: {
+      labels: [4.29],
+      stroke: {
+        lineCap: "round"
+      },
+      colors: ["black"],
+
+      fill: {
+        colors:['#219653']
+      },
+
+      plotOptions: {
+        radialBar: {
+          hollow: {
+            margin: 0,
+            size: "50%",
+          },
+          dataLabels: {
+            name: {
+              show: true,
+              color: "#000",
+              fontSize: "20px"
+            },
+            value: {
+              color: "green",
+              show: true,
+              fontSize: "16px",
+            }
+          }
+        }
+      }
+
+    },
+    series: [86],
+    
+    stroke: {
+      lineCap: "round"
+    },
+  }
+
+
   return (
     <div className={profileStyles.container}>
       <StudentInfo />
@@ -16,7 +60,21 @@ const Profile = () => {
           <StudentProfile/>
         </div>
 
-        <div className={profileStyles.calendar}></div>
+        <div className={profileStyles.calendarGPA}>
+          <div className={profileStyles.calendar}></div>
+          
+          <div className={profileStyles.gpa}>
+            <h3>Current CCGPA</h3>
+            <div className={profileStyles.containerThree}>
+              <p>Remark<br/> <span>Good standing</span></p>
+              <Chart 
+                options={data.options} 
+                series={data.series} 
+                type="radialBar" width="220" 
+              />
+            </div>
+          </div>
+        </div>
 
       </div>
     </div>
