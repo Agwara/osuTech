f1a879f977a1d99723279f752dd46ac8af004d53
agwara <nnaemekaagwara@gmail.com>
2021-10-19 14:43:33 +0100

    new update

--- src/components/MapContainer/MapContainer.jsx
+++ src/components/MapContainer/MapContainer.jsx
@@ -16,6 +16,13 @@ const MapContainer = () => {
 
   const zoom = 18
 
+  function test () {
+    console.log(process.env)
+    console.log(process.env.REACT_APP_GOOGLE_API_KEY)
+  }
+
+  test()
+
   return (
     <div className={styles.container}>
       <GoogleMapReact
